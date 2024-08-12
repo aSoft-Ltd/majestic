7123491037492ff0c28239820d95d53d7228387d
andylamax <andylamax@programmer.net>
2024-08-12 03:32:46 +0300

    Added majestic to the fold

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    includeBuild("../build-logic")
+}
+
+plugins {
+    id("multimodule")
+}
+
+fun includeSubs(base: String, path: String = base, vararg subs: String) {
+    subs.forEach {
+        include(":$base-$it")
+        project(":$base-$it").projectDir = File("$path/$it")
+    }
+}
+
+rootProject.name = "majestic"
+
+includeSubs("majestic-input", "input", "core")
