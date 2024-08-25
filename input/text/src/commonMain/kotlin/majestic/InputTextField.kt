f41c9d8cfcc2d2c9fff5b2d0f9fc4868c12dd9ef
Isaka Developer <isaxstarisaka@gmail.com>
2024-08-26 02:54:42 +0300

    update InputTextField

--- input/text/src/commonMain/kotlin/majestic/InputTextField.kt
+++ input/text/src/commonMain/kotlin/majestic/InputTextField.kt
@@ -1,24 +1,19 @@
 package majestic
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.VisualTransformation
@@ -26,7 +21,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cinematic.watchAsState
 import kollections.firstOrNull
-import neat.Invalid
 import neat.Valid
 import neat.Validity
 import symphony.BaseField
@@ -34,57 +28,7 @@ import symphony.toErrors
 
 @Composable
 fun InputTextField(
-    label: String,
-    value: String,
-    placeholder: String,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    visualTransformation: VisualTransformation = VisualTransformation.None,
-    trailingIcon: @Composable (() -> Unit)? = null,
-    leadingIcon: @Composable (() -> Unit)? = null,
-    onValueChange: (String) -> Unit
-) {
-    val colorBlue = Color(red = 0x00, green = 0x61, blue = 0xFF)
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 12.dp)
-    ) {
-        Text(
-            modifier = Modifier.padding(bottom = 8.dp),
-            text = label
-        )
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth(),
-            singleLine = true,
-            value = value,
-            leadingIcon = leadingIcon,
-            trailingIcon = trailingIcon,
-            keyboardOptions = keyboardOptions,
-            visualTransformation = visualTransformation,
-            placeholder = {
-                Text(
-                    text = placeholder,
-                    color = Color.Black.copy(alpha = 0.4f),
-                    fontSize = 16.sp
-                )
-            },
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                focusedIndicatorColor = colorBlue,
-                unfocusedIndicatorColor = Color.Black.copy(alpha = 0.2f),
-            ),
-            shape = RoundedCornerShape(8.dp),
-            onValueChange = { onValueChange(it) }
-        )
-    }
-}
-
-
-@Composable
-fun InputTextField(
+    modifier: Modifier = Modifier,
     field: BaseField<String>,
     label: String = field.label.capitalizedWithAstrix(),
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
@@ -94,9 +38,7 @@ fun InputTextField(
     onChange: ((String) -> Unit)? = null
 ) {
     val colorBlue = Color(red = 0x00, green = 0x61, blue = 0xFF)
-
     val state = field.state.watchAsState()
-
     var validity: Validity<Any?> by remember(state.output) { mutableStateOf(Valid(state.output)) }
 
     LaunchedEffect(state.output) {
@@ -104,9 +46,7 @@ fun InputTextField(
     }
 
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 12.dp)
+        modifier = modifier.fillMaxWidth()
     ) {
         Text(
             modifier = Modifier.padding(bottom = 8.dp),
@@ -125,7 +65,7 @@ fun InputTextField(
                 Text(
                     text = field.hint,
                     color = Color.Black.copy(alpha = 0.4f),
-                    fontSize = 16.sp
+                    fontSize = 17.sp
                 )
             },
             colors = TextFieldDefaults.colors(
@@ -140,9 +80,9 @@ fun InputTextField(
                 onChange?.invoke(it)
             }
         )
-        if (validity is Invalid) Text(
-            modifier = Modifier.padding(bottom = 8.dp),
+        Text(
             color = Color.Red,
+            fontSize = 12.sp,
             text = validity.toErrors().firstOrNull()?.message ?: ""
         )
     }
