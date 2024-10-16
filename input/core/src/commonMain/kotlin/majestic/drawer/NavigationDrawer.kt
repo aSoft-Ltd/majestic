a9aa2830261043ab063c1b8c3232e02b826a67fc
andylamax <andylamax@programmer.net>
2024-10-17 02:02:21 +0300

    Finished creating a panel

--- input/core/src/commonMain/kotlin/majestic/drawer/NavigationDrawer.kt
+++ input/core/src/commonMain/kotlin/majestic/drawer/NavigationDrawer.kt
@@ -2,11 +2,11 @@ package majestic.drawer
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -19,55 +19,30 @@ fun NavigationDrawer(
     drawer: @Composable (DrawerController) -> Unit,
     content: @Composable (DrawerController) -> Unit,
 ) {
-    val ratio by animateFloatAsState(if (controller.isOpen) controller.ratio else 0.0f, animationSpec = controller.animation)
+    val ratio by animateFloatAsState(
+        targetValue = if (controller.isOpen) controller.ratio else 0.0f,
+        animationSpec = controller.animation
+    )
 
     when {
         controller.overlap && controller.direction == DrawerOpenDirection.Right -> Box(modifier) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                content(controller)
-            }
-            Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(ratio)) {
-                drawer(controller)
-            }
+            Box(modifier = Modifier.fillMaxSize()) { content(controller) }
+            Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(ratio)) { drawer(controller) }
         }
 
-        controller.overlap && controller.direction == DrawerOpenDirection.Up -> {
-            Text("Come from down")
+        controller.overlap && controller.direction == DrawerOpenDirection.Up -> Box(modifier, contentAlignment = Alignment.BottomCenter) {
+            Box(modifier = Modifier.fillMaxSize()) { content(controller) }
+            Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(ratio)) { drawer(controller) }
         }
 
         !controller.overlap && controller.direction == DrawerOpenDirection.Right -> Row(modifier) {
-            Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(ratio)) {
-                drawer(controller)
-            }
-            Box(modifier = Modifier.fillMaxHeight().weight(1f - ratio)) {
-                content(controller)
-            }
+            Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(ratio)) { drawer(controller) }
+            Box(modifier = Modifier.fillMaxHeight().weight(1f - ratio)) { content(controller) }
         }
 
-        !controller.overlap && controller.direction == DrawerOpenDirection.Up -> Box(modifier, contentAlignment = Alignment.BottomCenter) {
-            Box(modifier = Modifier.fillMaxSize()) {
-                content(controller)
-            }
-            Box(
-                modifier = Modifier.fillMaxWidth().fillMaxHeight(ratio),
-            ) {
-                drawer(controller)
-            }
+        !controller.overlap && controller.direction == DrawerOpenDirection.Up -> Column(modifier) {
+            Box(modifier = Modifier.fillMaxWidth().weight(1f - ratio)) { content(controller) }
+            Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(ratio)) { drawer(controller) }
         }
     }
-//    if (!controller.overlap) Row(modifier) {
-//        Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(ratio)) {
-//            drawer(controller)
-//        }
-//        Box(modifier = Modifier.fillMaxHeight().weight(1f - ratio)) {
-//            content(controller)
-//        }
-//    } else Box(modifier) {
-//        Box(modifier = Modifier.fillMaxSize()) {
-//            content(controller)
-//        }
-//        Box(modifier = Modifier.fillMaxHeight().fillMaxWidth(ratio)) {
-//            drawer(controller)
-//        }
-//    }
 }
