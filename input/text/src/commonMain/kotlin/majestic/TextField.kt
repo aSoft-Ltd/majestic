bad55aaf92e7fa21df0a2be2041d2eb46a8f6fc8
ronaldchaula <2021-04-01176@udsm.ac.tz>
2024-11-21 09:21:08 +0300

    added ability to expand the textfield based on the number of maxlines

--- input/text/src/commonMain/kotlin/majestic/TextField.kt
+++ input/text/src/commonMain/kotlin/majestic/TextField.kt
@@ -2,7 +2,9 @@ package majestic
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
@@ -13,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cinematic.watchAsState
@@ -163,7 +166,7 @@ fun TextField(
     Column(modifier = modifier) {
         label()
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().then(if (maxLines > 56) Modifier.height(maxLines.dp) else Modifier.wrapContentHeight()),
             value = value,
             leadingIcon = leadingIcon,
             trailingIcon = trailingIcon,
