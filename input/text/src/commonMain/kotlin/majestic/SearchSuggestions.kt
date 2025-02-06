75478234e1b3807d1b8740e3d4ef1ba16f2add41
andylamax <andylamax@programmer.net>
2025-02-06 11:14:37 +0300

    Improved search suggestion component

--- input/text/src/commonMain/kotlin/majestic/SearchSuggestions.kt
+++ input/text/src/commonMain/kotlin/majestic/SearchSuggestions.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.window.PopupProperties
 import majestic.colors.ThemeColors
 
@@ -15,36 +14,24 @@ fun <T> SearchSuggestions(
     expanded: Boolean,
     suggestions: List<T>,
     theme: ThemeColors,
-    onItemSelected: (T) -> Unit,
-    onNavigate: (T) -> Unit,
+    onClick: (T) -> Unit,
     suggestion: @Composable (T) -> Unit,
-    modifier: Modifier
+    modifier: Modifier = Modifier
+) = DropdownMenu(
+    containerColor = theme.surface1.main.background,
+    expanded = expanded,
+    onDismissRequest = { /* Do nothing to prevent auto-dismiss */ },
+    modifier = modifier,
+    properties = PopupProperties(
+        focusable = false,
+        dismissOnBackPress = false,
+        dismissOnClickOutside = true
+    )
 ) {
-    val density = LocalDensity.current
-    DropdownMenu(
-        containerColor = theme.surface1.main.background,
-        expanded = expanded,
-        onDismissRequest = { /* Do nothing to prevent auto-dismiss */ },
-        modifier = modifier,
-        properties = PopupProperties(
-            focusable = false,
-            dismissOnBackPress = false,
-            dismissOnClickOutside = true
-        )
-    ) {
-        suggestions.forEachIndexed { index, item ->
-            DropdownMenuItem(
-                modifier = Modifier.fillMaxWidth()
-                    .background(theme.surface1.main.background),
-                onClick = {
-                    onItemSelected(item)
-                    onNavigate(item)
-                },
-                text = {
-                    suggestion(item)
-                }
-            )
-        }
-    }
+    for (item in suggestions) DropdownMenuItem(
+        modifier = Modifier.fillMaxWidth().background(theme.surface1.main.background),
+        onClick = { onClick(item) },
+        text = { suggestion(item) }
+    )
 }
 
