4d9153d5024af9079514d52516be4141add38e6a
ronaldchaula <2021-04-01176@udsm.ac.tz>
2025-02-06 08:29:59 +0300

    exposed the modifier and moved the maxHeight setup to external modifier implementation

--- input/text/src/commonMain/kotlin/majestic/SearchSuggestions.kt
+++ input/text/src/commonMain/kotlin/majestic/SearchSuggestions.kt
@@ -1,15 +1,12 @@
 package majestic
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.requiredSizeIn
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
 import majestic.colors.ThemeColors
 
@@ -17,21 +14,18 @@ import majestic.colors.ThemeColors
 fun <T> SearchSuggestions(
     expanded: Boolean,
     suggestions: List<T>,
-    containerWidth: Int,
     theme: ThemeColors,
     onItemSelected: (T) -> Unit,
     onNavigate: (T) -> Unit,
     suggestion: @Composable (T) -> Unit,
-    maxHeight: Dp = 350.dp
+    modifier: Modifier
 ) {
     val density = LocalDensity.current
     DropdownMenu(
         containerColor = theme.surface1.main.background,
         expanded = expanded,
         onDismissRequest = { /* Do nothing to prevent auto-dismiss */ },
-        modifier = Modifier.width(with(density) { containerWidth.toDp() })
-            .background(theme.surface1.main.background)
-            .requiredSizeIn(maxHeight = maxHeight),
+        modifier = modifier,
         properties = PopupProperties(
             focusable = false,
             dismissOnBackPress = false,
@@ -40,7 +34,7 @@ fun <T> SearchSuggestions(
     ) {
         suggestions.forEachIndexed { index, item ->
             DropdownMenuItem(
-                modifier = Modifier.width(with(density) { containerWidth.toDp() })
+                modifier = Modifier.fillMaxWidth()
                     .background(theme.surface1.main.background),
                 onClick = {
                     onItemSelected(item)
