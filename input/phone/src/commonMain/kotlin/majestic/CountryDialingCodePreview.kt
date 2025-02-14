6d3482b45df0f7316e17d81fc3ab0bfba747a966
Isaka Developer <isaxstarisaka@gmail.com>
2025-02-14 17:22:46 +0300

    update dropdown selector

--- input/phone/src/commonMain/kotlin/majestic/CountryDialingCodePreview.kt
+++ input/phone/src/commonMain/kotlin/majestic/CountryDialingCodePreview.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import nation.Country
 import nation.Flag
@@ -14,9 +15,10 @@ import nation.Flag
 @Composable
 fun CountryDialingCodePreview(
     country: Country,
+    color: Color = Color.Black,
     modifier: Modifier = Modifier
 ) = Row(modifier, verticalAlignment = Alignment.CenterVertically) {
     Flag(country)
     Spacer(modifier = Modifier.width(5.dp))
-    Text("+${country.dialingCode}")
+    Text("+${country.dialingCode}", color = color)
 }
