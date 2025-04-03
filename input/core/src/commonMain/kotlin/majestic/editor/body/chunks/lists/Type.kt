a3554ee9f3513126eaa1a70d8610b32954571512
ronaldchaula <2021-04-01176@udsm.ac.tz>
2025-04-03 17:23:19 +0300

    changed from sealed class to sealed interface on the Type sealed interface

--- input/core/src/commonMain/kotlin/majestic/editor/body/chunks/lists/Type.kt
+++ input/core/src/commonMain/kotlin/majestic/editor/body/chunks/lists/Type.kt
@@ -1,14 +1,14 @@
 package majestic.editor.body.chunks.lists
 
-sealed class Type {
-    sealed class Ordered : Type() {
-        data object Numbers : Ordered()
-        data class Alphabetic(val caps: Boolean) : Ordered()
-        data class Roman(val caps: Boolean) : Ordered()
+sealed interface Type {
+    sealed interface Ordered : Type {
+        data object Numbers : Ordered
+        data class Alphabetic(val caps: Boolean) : Ordered
+        data class Roman(val caps: Boolean) : Ordered
     }
-    sealed class UnOrdered : Type() {
-        data object Bullet : UnOrdered()
-        data object Arrow : UnOrdered()
-        data object Diamond : UnOrdered()
+    sealed interface UnOrdered : Type {
+        data object Bullet : UnOrdered
+        data object Arrow : UnOrdered
+        data object Diamond : UnOrdered
     }
 }
