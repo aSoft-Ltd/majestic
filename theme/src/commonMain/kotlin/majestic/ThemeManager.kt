6f0bfbd8353cd3c73a060e32f9cad1c50bc872ea
andylamax <andylamax@programmer.net>
2025-06-10 05:35:29 +0300

    Working on the theme manager

--- theme/src/commonMain/kotlin/majestic/ThemeManager.kt
+++ theme/src/commonMain/kotlin/majestic/ThemeManager.kt
@@ -1,8 +1,21 @@
 package majestic
 
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 
-class ThemeManager {
+class ThemeManager(
+    val choices: List<ColorPair> = listOf(
+        ColorPair(Color(0xFFFAFAFA), Color(0xFF0061FF)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFF673AB7)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFF26C6DA)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFF26A69A)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFF4CAF50)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFFCDDC39)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFFFFA000)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFFF57C00)),
+        ColorPair(Color(0xFFFAFAFA), Color(0xFFFF5722)),
+    )
+) {
     val state = mutableStateOf(ThemeState.dark())
 
     fun setMode(m: ThemeMode) {
@@ -14,4 +27,11 @@ class ThemeManager {
     }
 
     fun toggleMode() = setMode(state.value.mode.toggled())
+
+    fun setColor(color: ColorPair) {
+        val current = state.value
+        state.value = current.copy(
+            colors = current.colors.copy(primary = color)
+        )
+    }
 }
