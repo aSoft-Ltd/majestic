71c4b844e82c9f5f0eebe3c7fe58ce580ba6af3b
andylamax <andylamax@programmer.net>
2025-07-31 12:40:54 +0300

    Added LocalDrawerHostControllerContext

---
+++ drawers/src/commonMain/kotlin/majestic/drawer/LocalDrawerHostControllerContext.kt
@@ -0,0 +1,5 @@
+package majestic.drawer
+
+import androidx.compose.runtime.staticCompositionLocalOf
+
+val LocalDrawerHostControllerContext = staticCompositionLocalOf<MultiDrawerController?> { null }
