775b185f0d9e701be369d5d6a59b8b58c703053c
Isaka Developer <isaxstarisaka@gmail.com>
2025-11-06 13:15:28 +0300

    add users module

---
+++ users/build.gradle.kts
@@ -0,0 +1,77 @@
+@file:OptIn(ExperimentalComposeLibrary::class)
+
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+plugins {
+    id("com.android.library")
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.compose")
+    kotlin("plugin.compose")
+    id("tz.co.asoft.library")
+    alias(kotlinz.plugins.dokka)
+}
+
+description = "The users module"
+
+android {
+    namespace = "tz.co.asoft.academia.majestic.users"
+    compileSdk = androidx.versions.compile.sdk.get().toInt()
+    defaultConfig {
+        minSdk = 21 // because of the coil dependency has this as it's min sdk
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    androidTarget {
+        // compilations.all {
+        //    compileTaskProvider {
+        //        compilerOptions.jvmTarget = JvmTarget.JVM_17
+        //    }
+        // }
+    }
+
+    jvm {
+        tasks.withType<Test> {
+            useJUnitPlatform()
+        }
+    }
+    js(IR) {
+        browser()
+        nodejs()
+    }
+
+    wasmJs { browser() }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(compose.runtime)
+            api(compose.foundation)
+            api(compose.material3)?.because("We need to access LocalContentColor")
+            api(compose.materialIconsExtended)?.because("We need default icons for table")
+            api(libs.symphony.table)?.because("We need a table manager of some sorts")
+            api(libs.cinematic.live.compose)?.because("We need to watchAsState")
+            api(projects.majesticTheme)?.because("We need an instance of ColorPair")
+            api(projects.majesticScreen)?.because("We need access to NoRippleInteractionSource")
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kommander.coroutines)
+            implementation(compose.uiTest)
+        }
+
+        jvmTest.dependencies {
+            implementation(compose.desktop.currentOs)
+        }
+    }
+}
