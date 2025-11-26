4b236745a9ad3b4c815982669a5cc8cfaf7ae44e
Isaka Developer <isaxstarisaka@gmail.com>
2025-11-26 08:25:30 +0300

    payment filter date range option labels

--- payments/src/commonMain/kotlin/majestic/payments/labels/PaymentLabels.kt
+++ payments/src/commonMain/kotlin/majestic/payments/labels/PaymentLabels.kt
@@ -2,20 +2,23 @@ package majestic.payments.labels
 
 data class PaymentLabels(
     val header: DashboardHeaderLabels,
-    val summary: SummaryLabels
+    val summary: SummaryLabels,
+    val filter: FilterLabels
 ) {
     companion object {
         val english by lazy {
             PaymentLabels(
                 header = DashboardHeaderLabels.english,
-                summary = SummaryLabels.english
+                summary = SummaryLabels.english,
+                filter = FilterLabels.english
             )
         }
 
         val swahili by lazy {
             PaymentLabels(
                 header = DashboardHeaderLabels.swahili,
-                summary = SummaryLabels.swahili
+                summary = SummaryLabels.swahili,
+                filter = FilterLabels.swahili
             )
         }
     }
