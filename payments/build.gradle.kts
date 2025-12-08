a12cea6e425c07ddf63b163b36e2f215a7e6025e
andylamax <andylamax@programmer.net>
2025-12-09 02:28:09 +0300

    Managed to make academia authenticate properly using heimdal

--- payments/build.gradle.kts
+++ payments/build.gradle.kts
@@ -33,17 +33,17 @@ kotlin {
             useJUnitPlatform()
         }
     }
-    js(IR) {
-        browser()
-        nodejs()
-    }
+//    js(IR) {
+//        browser()
+//        nodejs()
+//    }
 
     wasmJs { browser() }
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-    macosX64()
-    macosArm64()
+//    macosX64()
+//    macosArm64()
 
     sourceSets {
         commonMain.dependencies {
