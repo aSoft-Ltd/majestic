2d2b3d0ec5e5bda82f2b361a0d2f0e6d146c2e73
andylamax <andylamax@programmer.net>
2025-12-31 04:27:17 +0300

    Updated ClientConfig file

--- screen/src/commonMain/kotlin/majestic/config/ClientConfig.kt
+++ screen/src/commonMain/kotlin/majestic/config/ClientConfig.kt
@@ -4,8 +4,25 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 class ClientConfig(
+    /**
+     * The API endpoint for the application
+     */
     val api: String,
+    /**
+     * The start page for the application
+     */
     val start: String? = null,
+    /**
+     * The directory to store files and configuration
+     */
+    val directory: String? = null,
+    /**
+     * The starting window configuration
+     */
     val window: StartWindow? = null,
+
+    /**
+     * The database file name
+     */
     val database: String = "database.db"
 )
