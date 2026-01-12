faec646836576494d0bf2cb2487f1c9b81c16512
andylamax <andylamax@programmer.net>
2026-01-12 11:21:36 +0300

    fixed majestic project configuration error

--- screen/build.gradle.kts
+++ screen/build.gradle.kts
@@ -89,6 +89,7 @@ kotlin {
 
         commonTest.dependencies {
             implementation(kotlin("test"))
+            implementation(libs.kiota.connection.http)
             implementation(libs.kommander.core)
             implementation(libs.kommander.coroutines)
         }
