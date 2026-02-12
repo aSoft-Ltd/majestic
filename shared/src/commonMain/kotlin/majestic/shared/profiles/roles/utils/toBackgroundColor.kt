9837faa5cf42ee76c62b627c7efbbc4bf0417e54
andylamax <andylamax@programmer.net>
2026-02-12 10:40:02 +0300

    Removed orientation related properties

--- shared/src/commonMain/kotlin/majestic/shared/profiles/roles/utils/toBackgroundColor.kt
+++ shared/src/commonMain/kotlin/majestic/shared/profiles/roles/utils/toBackgroundColor.kt
@@ -1,18 +1,10 @@
 package majestic.shared.profiles.roles.utils
 
-import composex.screen.orientation.Landscape
-import composex.screen.orientation.Portrait
-import composex.screen.orientation.ScreenOrientation
 import majestic.shared.profiles.roles.RoleColors
 
 internal fun RoleColors.toBackgroundColor(
-    orientation: ScreenOrientation,
     hovered: Boolean,
-) = when (orientation) {
-    is Landscape -> when (hovered) {
-        true -> item.backgrounds.landscape.focused
-        false -> item.backgrounds.landscape.unfocused
-    }
-
-    is Portrait -> item.backgrounds.portrait
+) = when (hovered) {
+    true -> item.background.focused
+    false -> item.background.unfocused
 }
