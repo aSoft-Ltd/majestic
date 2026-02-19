7e357bc93dea3d5dae60527279acb449f4589d31
Isaka Developer <isaxstarisaka@gmail.com>
2026-02-19 20:12:54 +0300

    payment invoice before diner

---
+++ payments/src/commonMain/kotlin/majestic/payments/invoice/detail/InvoiceInfo.kt
@@ -0,0 +1,88 @@
+package majestic.payments.invoice.detail
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import composex.screen.orientation.Landscape
+import composex.screen.orientation.Portrait
+import composex.screen.orientation.ScreenOrientation
+import majestic.ColorPair
+import majestic.payments.invoice.detail.tools.DetailRow
+import majestic.payments.invoice.detail.tools.InvoiceDetail
+import majestic.payments.invoice.detail.tools.InvoiceTotal
+import majestic.payments.labels.invoice.InvoiceLabels
+
+@Composable
+fun InvoiceInfo(
+    labels: InvoiceLabels,
+    colors: ColorPair,
+    orientation: ScreenOrientation,
+    detail: InvoiceDetail,
+    modifier: Modifier = Modifier
+) = Column(
+    modifier = modifier,
+    verticalArrangement = Arrangement.spacedBy(20.dp),
+    horizontalAlignment = Alignment.End
+) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier.weight(2.4f),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Text(
+                text = "Address",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 1.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = colors.foreground.copy(0.7f)
+            )
+            Text(
+                text = detail.address,
+                fontSize = 14.sp,
+                lineHeight = 18.sp,
+                color = colors.foreground
+            )
+        }
+        if (orientation is Landscape) Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            DetailRow(modifier = Modifier.fillMaxWidth(), label = "Invoice #", description = detail.invoiceNo, color = colors.foreground)
+            DetailRow(modifier = Modifier.fillMaxWidth(), label = "Issued Date", description = detail.issued, color = colors.foreground)
+            DetailRow(modifier = Modifier.fillMaxWidth(), label = "Due Date", description = detail.due, color = colors.foreground)
+        }
+    }
+    Column(
+        modifier = Modifier.width(if (orientation is Landscape) 300.dp else 200.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        DetailRow(modifier = Modifier.fillMaxWidth(), label = "Sub-Total", description = detail.subTotal, color = colors.foreground)
+        DetailRow(modifier = Modifier.fillMaxWidth(), label = "TAX", description = detail.tax, color = colors.foreground)
+        DetailRow(modifier = Modifier.fillMaxWidth(), label = "Disc%", description = detail.discount, color = colors.foreground)
+    }
+    InvoiceTotal(
+        modifier = Modifier.then(
+            if (orientation is Portrait) Modifier.fillMaxWidth()
+            else Modifier.width(310.dp).clip(RoundedCornerShape(10.dp))
+        ).padding(10.dp),
+        label = "Total",
+        amount = detail.total,
+        color = colors.foreground,
+        orientation = orientation
+    )
+}
