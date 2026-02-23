8674f104af196a9af6c1c2a25dea95cae449a203
Isaka Developer <isaxstarisaka@gmail.com>
2026-02-23 22:48:00 +0300

    merge from main

--- payments/src/commonMain/kotlin/majestic/payments/wallet/details/analytics/AnalyticCard.kt
+++ payments/src/commonMain/kotlin/majestic/payments/wallet/details/analytics/AnalyticCard.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import majestic.ColorPair
-import majestic.payments.tools.separator
+import majestic.tooling.separator
 
 @Composable
 internal fun AnalyticCard(
