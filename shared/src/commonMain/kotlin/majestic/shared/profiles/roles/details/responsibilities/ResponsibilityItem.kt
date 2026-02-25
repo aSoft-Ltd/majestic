2e0e3ddd981de44459ae408a7674662486a4b475
ronaldchaula <2021-04-01176@udsm.ac.tz>
2026-02-25 08:05:57 +0300

    finished the asked changes

--- shared/src/commonMain/kotlin/majestic/shared/profiles/roles/details/responsibilities/ResponsibilityItem.kt
+++ shared/src/commonMain/kotlin/majestic/shared/profiles/roles/details/responsibilities/ResponsibilityItem.kt
@@ -3,10 +3,13 @@ package majestic.shared.profiles.roles.details.responsibilities
 import androidx.compose.foundation.background
 import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import composex.screen.orientation.Landscape
