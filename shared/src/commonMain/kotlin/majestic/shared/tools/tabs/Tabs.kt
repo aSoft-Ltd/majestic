135c640a363f1a155e18f50f82a161073f0bab08
ronaldchaula <ronald.chaula@live.com>
2026-03-20 01:42:42 +0300

    improved Tabs

--- shared/src/commonMain/kotlin/majestic/shared/tools/tabs/Tabs.kt
+++ shared/src/commonMain/kotlin/majestic/shared/tools/tabs/Tabs.kt
@@ -1,5 +1,6 @@
 package majestic.shared.tools.tabs
 
+import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Arrangement
@@ -13,35 +14,55 @@ import androidx.compose.ui.unit.dp
 import captain.Navigator
 import majestic.tooling.onClick
 
+data class TabItem(val label: String, val path: String) {
+    fun isSelected(url: String): Boolean = path.dropLastSlash().endsWith(url)
+    private fun String.dropLastSlash() = if (endsWith("/")) dropLast(1) else this
+}
+
+
 @Composable
 fun Tabs(
     current: String,
     pages: List<TabItem>,
     colors: TabItemColors,
     navigator: Navigator,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+) = Tabs(
+    items = pages,
+    selected = pages.firstOrNull { it.isSelected(current) },
+    onSelect = { navigator.navigate(it?.path ?: "") },
+    label = { it?.label ?: "" },
+    colors = colors,
+    modifier = modifier,
+)
+
+
+@Composable
+fun <T> Tabs(
+    items: List<T>,
+    selected: T,
+    onSelect: (T) -> Unit,
+    label: (T) -> String,
+    colors: TabItemColors,
+    modifier: Modifier = Modifier,
 ) = Row(
     modifier = modifier,
     verticalAlignment = Alignment.CenterVertically,
     horizontalArrangement = Arrangement.spacedBy(4.dp)
 ) {
-    for (page in pages) {
+    for (item in items) {
         val interactionSource = remember { MutableInteractionSource() }
         val isHovered by interactionSource.collectIsHoveredAsState()
-        val selected = page.isSelected(current)
+        val isSelected = item == selected
         Item(
             modifier = Modifier
-                .item(selected = selected, isHovered = isHovered, colors = colors)
-                .onClick { navigator.navigate(page.path) },
-            label = page.label,
+                .hoverable(interactionSource)
+                .item(selected = isSelected, isHovered = isHovered, colors = colors)
+                .onClick { onSelect(item) },
+            label = label(item),
             colors = colors,
-            selected = selected,
+            selected = isSelected,
             isHovered = isHovered,
         )
     }
-}
-
-data class TabItem(val label: String, val path: String) {
-    fun isSelected(url: String): Boolean = path.dropLastSlash().endsWith(url)
-    private fun String.dropLastSlash() = if (endsWith("/")) dropLast(1) else this
 }
