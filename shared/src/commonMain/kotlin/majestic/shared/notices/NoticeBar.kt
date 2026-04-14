fcc5cf09dff21c5bc5b6ef0a71dca81de35ead04
Ronald Chaula <ronald.chaula@live.com>
2026-04-14 14:43:59 +0300

    changed from static button to dropdown

--- shared/src/commonMain/kotlin/majestic/shared/notices/NoticeBar.kt
+++ shared/src/commonMain/kotlin/majestic/shared/notices/NoticeBar.kt
@@ -2,94 +2,51 @@ package majestic.shared.notices
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import majestic.dropdown.Dropdown
+import majestic.dropdown.DropdownColors
+import majestic.dropdown.DropdownItem
 import majestic.icons.Res
-import majestic.icons.ic_close
 import majestic.icons.ic_megaphone
-import majestic.tooling.onClick
-import org.jetbrains.compose.resources.painterResource
+import majestic.shared.notices.noticebar.DefaultNoticeBarHeader
+import org.jetbrains.compose.resources.vectorResource
 
 data class NoticeBarColors(
     val tint: Color,
     val title: Color,
     val action: Color,
-    val actionTint: Color
-)
+    val dropdownColors: DropdownColors,
+
+    )
 
 @Composable
-fun NoticeBar(
+fun <T> NoticeBar(
     colors: NoticeBarColors,
-    onClose: () -> Unit = {},
-    onNotify: () -> Unit,
+    items: List<DropdownItem<T>> = emptyList(),
+    onNotify: (T) -> Unit = {},
     notify: String,
     modifier: Modifier = Modifier,
+    onClose: () -> Unit = {},
     header: @Composable () -> Unit = {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(20.dp, Alignment.Start),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_close),
-                contentDescription = "Close",
-                tint = colors.tint,
-                modifier = Modifier.size(18.dp).onClick(onClose)
-            )
-            Text(
-                text = "Notices",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold,
-                color = colors.title
-            )
-        }
+        DefaultNoticeBarHeader(colors, onClose)
     }
 ) = Row(
     modifier = modifier,
     verticalAlignment = Alignment.CenterVertically,
     horizontalArrangement = Arrangement.SpaceBetween
 ) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(20.dp, Alignment.Start),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        header()
-    }
+    header()
 
-    TextButton(
-        onClick = onNotify,
+    Dropdown(
+        items = items,
+        onAction = onNotify,
+        colors = colors.dropdownColors,
+        label = notify,
+        leadingIcon = vectorResource(Res.drawable.ic_megaphone),
         modifier = Modifier.wrapContentWidth()
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(Res.drawable.ic_megaphone),
-                contentDescription = notify,
-                tint = colors.actionTint,
-                modifier = Modifier.size(20.dp)
-            )
-
-            Text(
-                text = notify,
-                fontSize = 16.sp,
-                maxLines = 1,
-                lineHeight = 1.sp,
-                overflow = TextOverflow.Ellipsis,
-                fontWeight = FontWeight.Medium,
-                color = colors.action
-            )
-        }
-    }
+    )
 }
